21463660a04cd50a57dbc2b93d64b5a665b227d4
khanhh2101 <dangnuhongkhanh0209@gmail.com>
2026-07-12 18:45:20 +0700

    chore: ignore env files and remove test scripts

--- test_router.js
+++
@@ -1,8 +0,0 @@
-const { URLSearchParams } = require('url');
-const fn = (prev) => { return new URLSearchParams(); };
-try {
-  const params = new URLSearchParams(fn);
-  console.log("Success?", params.toString());
-} catch(e) {
-  console.log("Error:", e.message);
-}
